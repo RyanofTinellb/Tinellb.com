7e82edb15016b6c36caf5d0ad5646ef7d41d8668
RyanofTinellb <ryanoftinellb.@gmail.com>
2021-12-19 16:08:04 +1100

    Help with Cracking the Cryptic

---
+++ sudoku/thermo159/cell.js
@@ -0,0 +1,119 @@
+let thick = '2px';
+let layout = [
+  [0, 0, 0, 1, 1, 1, 2, 2, 2],
+  [0, 0, 0, 1, 1, 1, 2, 2, 2],
+  [0, 0, 0, 1, 1, 1, 2, 2, 2],
+  [3, 3, 3, 4, 4, 4, 5, 5, 5],
+  [3, 3, 3, 4, 4, 4, 5, 5, 5],
+  [3, 3, 3, 4, 4, 4, 5, 5, 5],
+  [6, 6, 6, 7, 7, 7, 8, 8, 8],
+  [6, 6, 6, 7, 7, 7, 8, 8, 8],
+  [6, 6, 6, 7, 7, 7, 8, 8, 8]
+]
+
+class Cell {
+  constructor(index) {
+    this.row = Math.floor(index / 9);
+    this.col = index % 9;
+    this.rgn = layout[this.row][this.col];
+
+    this.setupMode = document.getElementById('setupMode');
+
+    // let sqrrow = Math.floor(this.row / 3) % 3;
+    // let sqrcol = Math.floor(this.col / 3) % 3;
+    // this.sqr = 3 * sqrrow + sqrcol;
+
+    // sqrrow = this.row % 3;
+    // sqrcol = this.col % 3;
+    // this.ind = 3 * sqrrow + sqrcol;
+
+    this.sid = 's' + index;
+
+    this.possibilities = [];
+    for (let i = 0; i < 9; i++) {
+      this.possibilities.push(true);
+    }
+
+    this.cell = document.getElementById(this.sid);
+    this.setup();
+  }
+
+  contents() {
+    content = '';
+    for (let i = 0; i < 9; i++) {
+      if (this.possibilities[i]) {
+        content += (i + 1).toString();
+      }
+    }
+    return content;
+  };
+
+  borders() {
+    if (this.row == 0) {
+      this.cell.style.borderTopWidth = thick;
+    }
+    if (this.row == 8) {
+      this.cell.style.borderBottomWidth = thick;
+    }
+    if (this.col == 0) {
+      this.cell.style.borderLeftWidth = thick;
+    }
+    if (this.col == 8) {
+      this.cell.style.borderRightWidth = thick;
+    }
+    if (layout[Math.max(0, this.row - 1)][this.col] != this.rgn) {
+      this.cell.style.borderTopWidth = thick;
+    }
+    if (layout[Math.min(8, this.row + 1)][this.col] != this.rgn) {
+      this.cell.style.borderBottomWidth = thick;
+    }
+    if (layout[this.row][Math.max(0, this.col - 1)] != this.rgn) {
+      this.cell.style.borderLeftWidth = thick;
+    }
+    if (layout[this.row][Math.min(8, this.col + 1)] != this.rgn) {
+      this.cell.style.borderRightWidth = thick;
+    }
+  };
+
+  setup() {
+    this.borders();
+    this.update();
+  };
+
+  update() {
+    this.resize();
+    this.cell.innerHTML = this.contents();
+  };
+
+  resize() {
+    if (this.contents().length == 1) {
+      this.cell.style.fontSize = 'xx-large';
+    } else if (this.contents().length >= 6) {
+      this.cell.style.fontSize = 'small';
+    } else {
+      this.cell.style.fontSize = 'large';
+    }
+  };
+
+  set(number) {
+    number--;
+    for (let i = 0; i < 9; i++) {
+      this.possibilities[i] = (i == number);
+    }
+  };
+
+  select() {
+    this.selected = true;
+    if (this.setupMode.checked) {
+      this.cell.style.backgroundColor = 'red';
+    } else {
+      this.cell.style.backgroundColor = 'yellow';
+    }
+  };
+
+  deselect() {
+    this.selected = false;
+    this.cell.style.backgroundColor = 'white';
+  };
+
+}
---
+++ sudoku/thermo159/game.js
@@ -0,0 +1,200 @@
+class Game {
+  constructor() {
+    this.cells = [];
+    this.rows = [];
+    this.cols = [];
+    this.rgns = [];
+    this.highlighted = Array(9).fill(false);
+    this.colors = ['red', 'orange', 'blue', 'green', 'purple'];
+    this.color = this.colors[Math.floor(5 * Math.random())];
+    this.setupMode = document.getElementById('setupMode');
+    for (let i = 0; i < 81; i++) {
+      this.cells.push(new Cell(i));
+    }
+    for (let row = 0; row < 9; row++) {
+      let house = new House(this.cells.filter(cell => cell.row == row));
+      this.rows.push(house);
+    }
+    for (let col = 0; col < 9; col++) {
+      let house = new House(this.cells.filter(cell => cell.col == col));
+      this.cols.push(house);
+    }
+    for (let rgn = 0; rgn < 9; rgn++) {
+      let house = new House(this.cells.filter(cell => cell.rgn == rgn));
+      this.rgns.push(house);
+    }
+    this.selected = this.cells[0];
+  }
+  set(cell, number) {
+    number--;
+    cell.set(number + 1);
+    this.cleanCousins(cell);
+    this.update();
+  };
+  setup(arr) {
+    for (let i = 0; i < 81; i++) {
+      row = Math.floor(i / 9);
+      col = i % 9;
+      if (arr[i] != 0) {
+        this.set(row, col, arr[i]);
+      }
+    }
+    this.update();
+  };
+  update() {
+    for (let cell of this.cells) {
+      cell.update();
+    }
+  };
+  click(event) {
+    let id = event.target.id;
+    for (let cell of this.cells) {
+      if (cell.sid == id) {
+        cell.select();
+        this.selected = cell;
+      } else {
+        cell.deselect();
+      }
+    }
+  };
+  keypress(event) {
+    let row;
+    let col;
+    let number = parseInt(event.key);
+    if (event.key == 'ArrowUp') {
+      row = (this.selected.row + 8) % 9;
+      col = this.selected.col;
+      this.select(this.rows[row].cells[col]);
+      event.preventDefault();
+    } else if (event.key == 'ArrowDown') {
+      row = (this.selected.row + 1) % 9;
+      col = this.selected.col;
+      this.select(this.rows[row].cells[col]);
+      event.preventDefault();
+    } else if (event.key == 'ArrowLeft') {
+      col = (this.selected.col + 8) % 9;
+      row = this.selected.row;
+      this.select(this.rows[row].cells[col]);
+      event.preventDefault();
+    } else if (event.key == 'ArrowRight') {
+      col = (this.selected.col + 1) % 9;
+      row = this.selected.row;
+      this.select(this.rows[row].cells[col]);
+      event.preventDefault();
+    } else if (event.key == "+" && this.selected.contents().length == 1) {
+      this.cleanCousins(this.selected);
+    } else if (number != 0 && !isNaN(number)) {
+      if (this.setupMode.checked) {
+        this.set(this.selected, number);
+      } else {
+        this.selected.possibilities[number - 1] = false;
+        if (this.selected.contents().length == 1) {
+          this.cleanCousins(this.selected);
+        }
+      }
+      this.selected.update();
+      this.ensureSwap(this.selected)
+      this.keeplit();
+    }
+  };
+  keySelect(number) {
+    if (this.setupMode.checked) {
+      this.set(this.selected, number);
+    } else {
+      this.selected.possibilities[number - 1] = false;
+      if (this.selected.contents().length == 1) {
+        this.cleanCousins(this.selected);
+      }
+    }
+    this.selected.update();
+  };
+
+  select(square) {
+    for (let cell of this.cells) {
+      if (cell.sid == square.sid) {
+        cell.select();
+        this.selected = cell;
+      } else {
+        cell.deselect();
+      }
+    }
+  };
+
+  cleanCousins(cell) {
+    this.findCousins(cell).forEach(square => {
+      let number = parseInt(cell.contents()) - 1;
+      let len = square.contents().length;
+      square.possibilities[number] = false;
+      if (len == 2 && square.contents().length == 1) {
+        this.cleanCousins(square);
+        this.ensureSwap(square)
+      }
+      square.update();
+    });
+  };
+
+  ensureSwap(cell) {
+    let row = cell.row;
+    if (cell.contents().length == 1) {
+      let col = parseInt(cell.contents() - 1);
+      let sqr = this.rows[row].cells[col];
+      if (sqr.contents().length != 1) {
+        this.set(sqr, cell.col + 1)
+      }
+    } else {
+      console.log(cell.contents())
+      cell.possibilities.forEach((elt, col) => {
+        if (!elt) {
+          let sqr = this.rows[row].cells[col]
+          let len = sqr.contents().length;
+          sqr.possibilities[cell.col] = false;
+          sqr.update();
+          if (len == 2 && sqr.contents().length == 1) {
+            this.cleanCousins(sqr);
+          }
+        }
+      });
+    }
+  }
+
+  highlight(number) {
+    this.color = this.colors[Math.floor(5 * Math.random())];
+    if (this.highlighted[number - 1]) {
+      this.unlight(number);
+    } else {
+      this.relight(number);
+    }
+  };
+
+  relight(number) {
+    this.highlighted[number - 1] = true;
+    for (let cell of this.cells) {
+      cell.cell.innerHTML = cell.contents().replace(number, '<span class="light">' + number + '</span>');
+    }
+    let styleblock = document.getElementById('style');
+    let style = styleblock.innerHTML;
+    for (let clr of this.colors) {
+      style = style.replace(clr, this.color);
+    }
+    styleblock.innerHTML = style;
+  }
+
+  unlight(number) {
+    this.highlighted[number - 1] = false;
+    for (let cell of this.cells) {
+      cell.cell.innerHTML = cell.contents().replace('<span class="light">' + number + '</span>', number);
+    }
+  }
+
+  keeplit() {
+    this.highlighted.forEach((v, i) => {
+      if (v) {
+        this.relight(i + 1)
+      }
+    })
+  }
+
+  findCousins(square) {
+    return this.cells.filter(cell => ((cell.row == square.row || cell.col == square.col || cell.rgn == square.rgn) && cell.sid != square.sid));
+  };
+}

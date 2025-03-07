2f5be942f1cbdbce4574e6b151b80c14e8287176
RyanofTinellb <ryanoftinellb@gmail.com>
2025-03-07 13:10:16 +1100

    Highlight text properly without damaging tags

--- data/scripts/parse_href.js
+++ data/scripts/parse_href.js
@@ -1,17 +1,23 @@
 searchParams = new URLSearchParams(window.location.search);
 let text = searchParams.get('highlight');
+
+function bold(elt, str) {
+    let bolden = RegExp('(\\W|^)(' + `${str}` + ')(\\W|$)', 'ig');
+    if (!elt.childElementCount) {
+        elt.innerHTML = elt.innerHTML.replace(bolden, `$1<span id="highlight" class="highlight">$2</span>$3`);
+        return;
+    } 
+    Array.from(elt.children).forEach(child => bold(child, str));
+}
+
 if (text) {
     let h1 = document.getElementsByTagName('h1')[0];
     h1.innerHTML += ' <a href="#highlight">→</a>'
-
     let main = document.getElementsByTagName('main')[0];
-    console.log(main);
-    let reg = RegExp('(\\W)(' + `${text}` + ')(\\W)', 'gi');
-    console.log(reg);
-    main.innerHTML = main.innerHTML.replace(reg, `$1<span id="highlight" class="highlight">$2</span>$3`);
+    bold(main, text);
 }
 
-let term = searchParams.get('term') 
+let term = searchParams.get('term')
 if (term) {
-    window.location.href = `/special/search.html?term=${term}`; 
+    window.location.href = `/special/search.html?term=${term}`;
 }
